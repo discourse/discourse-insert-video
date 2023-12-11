268790926f33c43b6fffa89fe34f37d472afa22c
Kelv <kelv@discourse.org>
2023-12-11 17:12:30 +0800

    DEV: upgrade insert video modal (#28)
    * DEV: upgraded insert-video modal
    * DEV: improve system test with data-test custom attribute
    * DEV: refactor preventSubmit to use ember modifier, remove redundant styling and update to call components with angle bracket syntax

---
+++ javascripts/discourse/components/modal/insert-video.js
@@ -0,0 +1,103 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { empty } from "@ember/object/computed";
+import { isVideo } from "discourse/lib/uploads";
+import I18n from "I18n";
+
+export default class InsertVideoModal extends Component {
+  @tracked sources;
+  @tracked tracks;
+  @tracked poster;
+  @empty("sources") insertDisabled;
+
+  _keyDown(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    }
+  }
+  @action
+  async preventSubmitOnEnter(modal) {
+    // prevent submitting on enter while adding items to lists using Enter
+    modal
+      .querySelector(".video-sources")
+      .addEventListener("keydown", this._keyDown);
+
+    modal
+      .querySelector(".video-subtitles")
+      .addEventListener("keydown", this._keyDown);
+  }
+
+  get trackList() {
+    if (!this.tracks) {
+      return [];
+    }
+
+    return this.tracks.split("|");
+  }
+
+  get sourceList() {
+    if (!this.sources) {
+      return [];
+    }
+
+    return this.sources.split("|");
+  }
+
+  get validationMessage() {
+    const allVideos = this.sourceList.every(
+      (url) => isVideo(url) || url.endsWith(".m3u8")
+    );
+
+    return allVideos ? "" : I18n.t(themePrefix("source_not_video"));
+  }
+
+  _sourceType(src) {
+    let prefix, type;
+    if (src.endsWith(".m3u8")) {
+      prefix = "application";
+      type = "x-mpegURL";
+    } else {
+      prefix = "video";
+      type = src.substring(src.lastIndexOf(".") + 1);
+    }
+    return `${prefix}/${type}`;
+  }
+
+  @action
+  insertVideo() {
+    let sources = "";
+    let tracks = "";
+
+    this.sourceList.forEach((src) => {
+      sources += `\n  <source src="${src}" type="${this._sourceType(src)}" />`;
+    });
+
+    const controlslist = settings?.disable_download
+      ? `controlslist="nodownload" `
+      : "";
+
+    this.trackList.forEach((t, i) => {
+      const track = t.split(",");
+      const url = track[0];
+      const label = track[1] || "English";
+      const langcode = track[2] || "en";
+      const def = i === 0 ? " default" : "";
+
+      tracks += `\n  <track src="${url}" label="${label}" kind="subtitles" srclang="${langcode}"${def}/>`;
+    });
+
+    const poster = this.poster ? ` poster="${this.poster}"` : "";
+    const text = `<video controls ${controlslist} preload="metadata"${poster}>${sources}${tracks}\n</video>`;
+    this.args.model.toolbarEvent.addText(text);
+
+    this.args.closeModal();
+  }
+
+  @action
+  setPoster(val) {
+    this.poster = val;
+  }
+}
--- spec/system/page_objects/modals/insert_video.rb
+++ spec/system/page_objects/modals/insert_video.rb
@@ -5,7 +5,6 @@ require "#{Rails.root}/spec/system/page_objects/modals/base"
 module PageObjects
   module Modals
     class InsertVideo < PageObjects::Modals::Base
-      BODY_SELECTOR = ".insert-video"
       MODAL_SELECTOR = ".insert-video-modal"
       def click_add_video_source_button
         find(".video-sources .simple-list-uploader .btn").click
@@ -19,8 +18,8 @@ module PageObjects
         find(".video-subtitles .simple-list-uploader .btn").click
       end
 
-      def click_primary_button
-        find(".modal-footer .btn-primary").click
+      def click_insert_video_button
+        footer.find('[data-test-id="insert-video-button"]').click
       end
     end
   end
