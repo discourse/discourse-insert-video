db8752d5c15c65c9fb26204d9bd117f9f0f666db
Kelvin Tan <kelv@discourse.org>
2023-12-29 12:29:27 +0800

    FIX: add assertions against modal input fields in system test to ensure uploads are done

--- spec/system/insert_video_spec.rb
+++ spec/system/insert_video_spec.rb
@@ -17,17 +17,11 @@ RSpec.describe "Inserting Video from Composer", system: true do
   it "should upload video" do
     SiteSetting.authorized_extensions += "|mp4|vtt"
     video_file =
-      File.absolute_path(
-        Pathname.new("#{__FILE__}/../../fixtures/media/sample_video.mp4")
-      )
+      File.absolute_path(Pathname.new("#{__FILE__}/../../fixtures/media/sample_video.mp4"))
     poster_file =
-      File.absolute_path(
-        Pathname.new("#{__FILE__}/../../fixtures/images/poster_small.jpg")
-      )
+      File.absolute_path(Pathname.new("#{__FILE__}/../../fixtures/images/poster_small.jpg"))
     subtitle_file =
-      File.absolute_path(
-        Pathname.new("#{__FILE__}/../../fixtures/media/sample_video.vtt")
-      )
+      File.absolute_path(Pathname.new("#{__FILE__}/../../fixtures/media/sample_video.vtt"))
 
     visit "/new-topic"
     expect(composer).to be_opened
@@ -35,9 +29,16 @@ RSpec.describe "Inserting Video from Composer", system: true do
     composer.click_toolbar_button("insertVideo")
     expect(insert_video_modal).to be_open
 
-    attach_file(video_file) { insert_video_modal.click_add_video_source_button }
+    # the relative order of the poster-related setup and assertion here should not
+    # be changed; it relies on the other input fields getting found by the Capybara
+    # finder methods first before we assert against the poster input's value
+    # (its actual text attribute is never updated in its current implementation)
     attach_file(poster_file) { insert_video_modal.click_add_poster_button }
+    attach_file(video_file) { insert_video_modal.click_add_video_source_button }
     attach_file(subtitle_file) { insert_video_modal.click_add_subtitle_button }
+    expect(insert_video_modal.video_source_input_field[:title]).to include "mp4"
+    expect(insert_video_modal.subtitle_input_field[:title]).to include "vtt"
+    expect(insert_video_modal.poster_input_field.value).to include ".jpeg"
     insert_video_modal.click_insert_video_button
 
     video_preview = composer.preview.find("video[controls][poster]")
