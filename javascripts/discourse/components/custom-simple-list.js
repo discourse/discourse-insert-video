c05d678d2e61ff957267d1082306a6d1adc3d1f8
David Taylor <david@taylorhq.com>
2024-12-02 13:29:35 +0000

    DEV: Convert to native class syntax (#53)

--- javascripts/discourse/components/custom-simple-list.js
+++ javascripts/discourse/components/custom-simple-list.js
@@ -1,18 +1,21 @@
 import Component from "@ember/component";
 import { action } from "@ember/object";
-import { empty, reads } from "@ember/object/computed";
+import { empty } from "@ember/object/computed";
+import { classNameBindings } from "@ember-decorators/component";
 import { on } from "discourse-common/utils/decorators";
 
-export default Component.extend({
-  classNameBindings: [":value-list"],
-  inputInvalid: empty("newValue"),
-  inputDelimiter: null,
-  newValue: "",
-  collection: null,
-  values: null,
-  placeholderKey: empty("placeholderKey")
-    ? themePrefix("simple_list_placeholder")
-    : reads("placeholderKey"),
+@classNameBindings(":value-list")
+export default class CustomSimpleList extends Component {
+  @empty("newValue") inputInvalid;
+
+  inputDelimiter = null;
+  newValue = "";
+  collection = null;
+  values = null;
+
+  get resolvedPlaceholderKey() {
+    return this.placeholderKey || themePrefix("simple_list_placeholder");
+  }
 
   @on("didReceiveAttrs")
   _setupCollection() {
@@ -21,19 +24,19 @@ export default Component.extend({
       "collection",
       this._splitValues(values, this.inputDelimiter || "\n")
     );
-  },
+  }
 
   keyUp(e) {
     if (e.keyCode === 13) {
       this.send("addValue", this.newValue);
     }
-  },
+  }
 
   @action
   changeValue(index, event) {
     const newValue = event.target.value;
     this._replaceValue(index, newValue);
-  },
+  }
 
   @action
   addValue(newValue) {
@@ -43,37 +46,37 @@ export default Component.extend({
 
     this.set("newValue", null);
     this._addValue(newValue);
-  },
+  }
 
   @action
   removeValue(value) {
     this._removeValue(value);
-  },
+  }
 
   @action
   addUploadUrl(value) {
     this.set("newValue", null);
     this._addValue(value);
-  },
+  }
 
   _addValue(value) {
     this.collection.addObject(value);
     this._saveValues();
-  },
+  }
 
   _removeValue(value) {
     this.collection.removeObject(value);
     this._saveValues();
-  },
+  }
 
   _replaceValue(index, newValue) {
     this.collection.replace(index, 1, [newValue]);
     this._saveValues();
-  },
+  }
 
   _saveValues() {
     this.set("values", this.collection.join(this.inputDelimiter || "\n"));
-  },
+  }
 
   _splitValues(values, delimiter) {
     if (values && values.length) {
@@ -81,5 +84,5 @@ export default Component.extend({
     } else {
       return [];
     }
-  },
-});
+  }
+}
